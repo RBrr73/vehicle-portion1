aab9b20f51598df4838e4c7d4b868f2864fe9b68
Cal P <31290760+Prof222@users.noreply.github.com>
2017-08-25 14:36:20 -0400

    Update movingsim.py

--- nmin/movingsim.py
+++ nmin/movingsim.py
@@ -103,7 +103,7 @@ class SimAcceleration(Driver):
                 
             
             # If RPM goes over MAX RPM, shift gears
-            # (i.e.: shift at redline)
+            # (i.e.: shift at redline or just before)
             if overspeed:
                 gear += 1
                 self.set_parameter_by_name('gear', gear)
@@ -124,6 +124,8 @@ class SimAcceleration(Driver):
             velocity += (acceleration*self.timestep)
         
             time += self.timestep
+            
+            check == time(self.speed)
                    
         self.accel_time = time
 
