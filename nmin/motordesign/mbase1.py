b761913f2316c2a63501d70ebf50d2c572fb6716
Rbr73 <30369980+Rbr73@users.noreply.github.com>
2017-08-29 09:08:22 -0400

    Add files via upload

---
+++ nmin/motordesign/mbase1.py
@@ -0,0 +1,46 @@
+const int analogInPin=A0;
+const int analogOutPin1=11;
+const int analogOutPin2=9;
+const int analogOutPin3=10;
+
+int val = 0; 
+int val2 = 0;
+int mag = 0;
+
+
+void setup(){
+
+Serial.begin(115200);
+}
+
+void loop(){
+  
+val = analogRead(analogInPin); 
+val2 = val-512;
+mag = abs(val2/2);
+if(mag>255){
+  mag = 255;
+}
+else{
+  mag = mag;
+}
+  
+ if(val2<-20){
+   digitalWrite(analogOutPin2,LOW);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin3,HIGH);
+ }
+ else if(val2>20){
+   digitalWrite(analogOutPin3,LOW);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin2,HIGH);
+ }
+ 
+
+ 
+ Serial.print(val);
+ Serial.print("\t");
+ Serial.println(mag);
+ 
+ delay(2);
+}
