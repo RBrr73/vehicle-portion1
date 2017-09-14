d2c619078b0689c373fa57c45bf22ebbf9fbf4e9
Rbr73 <30369980+Rbr73@users.noreply.github.com>
2017-09-13 20:42:30 -0400

    Update optimization.py

--- nmin/motordesign/optimization.py
+++ nmin/motordesign/optimization.py
@@ -49,6 +49,7 @@ class EngineOptimization(Assembly):
         self.driver.add_parameter('vehicle.spark_angle', -50., 10.)
         self.driver.add_parameter('vehicle.bore', 65., 100.)
         self.driver.add_parameter('vehicle.atrest', 3., 7.)
+        self.driver.add_parameter('vehicle.tis', 76.)
         self.driver.add_parameter('vehicle.resistance', 0, 16.)
         
         # Acceleration Sim setup
